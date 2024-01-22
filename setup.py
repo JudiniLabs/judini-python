13c7e647180a1c534ca9fa39c468a4f498faa590
Dani <dan.avila7@gmail.com>
2024-01-22 16:20:04 -0500

    setup fixed 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="judini",
-    version="0.1.0",
+    version="0.1.1",
     author="Judini Inc.",
     author_email="daniel@judini.ai",
     description="CodeGPT python package",
