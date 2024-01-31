60eed3e145a2324b7941d73730e8f87386bfb4c1
Vokturz <victornavarrokp47@gmail.com>
2024-01-31 11:32:15 -0300

    updated Completion.create method Now supports stream=True|False and format="json"|"text"

--- src/judini/codegpt/chat.py
+++ src/judini/codegpt/chat.py
@@ -1,6 +1,6 @@
-import http
 import json
 import requests
+from typing import List, Dict, Literal
 
 base_url = "https://api-beta.codegpt.co/api/v1"
 
@@ -8,48 +8,60 @@ class Completion:
     def __init__(self, api_key):
         self.api_key = api_key
 
+    def create(self, agent_id: str, messages: List[Dict[str, str]], 
+               stream: bool = False, format: Literal['json', 'text'] = 'text') -> str:
+        headers = {
+            "Content-Type": "application/json",
+            "media_type": "text/event-stream",
+            "Authorization": f"Bearer {self.api_key}"
+        }
 
-    def create(self, agent_id, messages, stream=False):
-            headers = {
-                "Content-Type": "application/json",
-                "media_type": "text/event-stream",
-                "Authorization": f"Bearer {self.api_key}"
-            }
+        payload = json.dumps({
+            "agentId": agent_id,
+            "messages": messages,
+            "stream": stream,
+            "format": "json" # By default always json
+        })
 
-            payload = json.dumps({
-                "agentId": agent_id,
-                "messages": messages
-            })
+        response = requests.post(f"{base_url}/chat/completions", headers=headers,
+                                  data=payload, stream=stream)
 
-            conn = http.client.HTTPSConnection("api-beta.codegpt.co")
-            url = "/api/v1/chat/completions"
-
-            conn.request("POST", url, body=payload, headers=headers)
-            res = conn.getresponse()
-            try:
-                data = res.read()
-            except Exception as e:
-                print(f"An error occurred: {e.__class__.__name__}")
-                print(f"Error details: {str(e)}")
-
-            content_data = ""
-            if stream is False:
-                data_array = data.decode("utf-8").replace('\n','').split('data: ')[1:]
-                for jd_str in data_array:
-                    if jd_str:
-                        try:
-                            # data: {}
-                            json_data = json.loads(jd_str)
+        if stream:
+            return self._handle_stream(response, format)
+        else:
+            return self._handle_non_stream(response, format)
+        
+    def _handle_stream(self, response: requests.Response,
+                       format: Literal['json', 'text']) -> List[str]:
+        try:
+            for chunk in response.iter_lines():
+                if chunk:
+                    chunk_str = chunk.decode("utf-8")
+                    data_array = chunk_str.replace('\n','').split('data: ')[1:]
+                    for jd_str in data_array:
+                        if jd_str == '[DONE]':
+                            break
+                        json_data = json.loads(jd_str)
+                        if format == 'json':
+                            yield json_data
+                        elif format == 'text':
                             for item in json_data['choices']:
-                                content_data += item['delta']['content']
-                        except:
-                            # data: [DONE]
-                            pass
+                                yield item['delta']['content']
+        except Exception as e:
+            print(f"Error occurred: {e}", jd_str)
+        finally:
+            response.close()
 
-                return content_data
-            else:
-                try:
-                    return data.decode("utf-8").replace('\n','').split('data: ')[1:]
-                except Exception as e:
-                    print(f"An error occurred: {e.__class__.__name__}")
-                    print(f"Error details: {str(e)}")
+    def _handle_non_stream(self, response: requests.Response,
+                           format: Literal['json', 'text']) -> str:
+        try:
+            json_data = response.json()
+            if format == 'json':
+                return json_data
+            elif format == 'text':
+                return json_data['choices'][0]['message']['content']
+        except Exception as e:
+            print(f"Error occurred: {e}")
+        finally:
+            response.close()
+        
