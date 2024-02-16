6a6f1c5f119af4b5403e5d810e014798380fa840
Vokturz <victornavarrokp47@gmail.com>
2024-02-16 12:47:09 -0300

    version 0.1.10

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="judini",
-    version="0.1.9",
+    version="0.1.10",
     author="Judini Inc.",
     author_email="daniel@judini.ai",
     description="CodeGPT python package",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.5",
+    python_requires=">=3.7",
     install_requires=[
         "requests", "aiohttp", "asyncio", "python-dotenv", "pydantic"
     ],
