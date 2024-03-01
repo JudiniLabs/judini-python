dacd0bed1fae9ffdc58b3146fac1a837800fbe4a
Vokturz <victornavarrokp47@gmail.com>
2024-03-01 11:18:16 -0300

    update chat_completion example

--- examples/chat_completion.py
+++ examples/chat_completion.py
@@ -1,36 +1,28 @@
 # Import necessary modules and libraries
 import os
-import asyncio
-from judini.codegpt.agent import Agent
-from dotenv import load_dotenv  # For loading environment variables from a .env file
+from judini import CodeGPTPlus
+from dotenv import load_dotenv
 
 # Load environment variables from a .env file if available
 load_dotenv()
 
 
-# Define an asynchronous function to demonstrate a chat interaction with a CodeGPT agent
+# Define a function to demonstrate a chat interaction with a CodeGPT agent
 
-async def chat_example(prompt):
+def chat_example(messages: list):
     # Retrieve the CodeGPT API key from environment variables
     CODEGPT_API_KEY = os.getenv("CODEGPT_API_KEY")
-
-    # Retrieve the CodeGPT agent ID from environment variables (or you can provide it directly)
-    # You can also specify the agent ID directly
+    CODEGPT_ORG_ID = os.getenv("CODEGPT_ORG_ID")
     CODEGPT_AGENT_ID = os.getenv("CODEGPT_AGENT_ID")
+    # Create an instance of the CodeGPTPlus class
+    codegpt = CodeGPTPlus(api_key=CODEGPT_API_KEY, org_id=CODEGPT_ORG_ID)
 
-    print(CODEGPT_API_KEY)
-    print(CODEGPT_AGENT_ID)
-    # Create an instance of the CodeGPT agent using the API key and agent ID
-    agent_instance = Agent(api_key=CODEGPT_API_KEY, agent_id=CODEGPT_AGENT_ID)
-
-    # Use an asynchronous loop to interact with the agent and get responses
-    async for response in agent_instance.chat_completion(prompt, stream=True):
-        print(response)  # Print the responses obtained from the agent
+    # Use a loop to interact with the agent and get responses
+    for chunk in codegpt.chat_completion(agent_id=CODEGPT_AGENT_ID, messages=messages, stream=True):
+        print(chunk, end="")  # Print the responses obtained from the agent
 
 # Entry point of the script
 if __name__ == "__main__":
     text = "First President of USA?"  # Define a user message for the conversation
-    prompt = {"role": "user", "content": text}  # Create a prompt for the user
-
-    # Run the chat_example function with the user's prompt using asyncio
-    asyncio.run(chat_example(prompt))
+    message = {"role": "user", "content": text}  # Create a message for the user
+    chat_example([message])
